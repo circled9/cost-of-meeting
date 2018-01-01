34ae6b2d38ab2cd03cee0971dcdeccf7fb12a88a
Matsui Masashi <matsui@kaffeine.jp>
2018-01-01 10:22:14 +0900

    月単価・時給の設定ができるようにした

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import './App.css'
 
 class App extends Component {
@@ -9,8 +9,11 @@ class App extends Component {
       hourPerDay: 8,
       now: null,
       numberOfPeople: 1,
+      showSetting: false,
       startTime: null,
-      unitPrice: 10000000,
+      unitPricePerDay: 1000,
+      unitPricePerMonth: 1000000,
+      unitPriceType: 'perMonth',
     }
 
     this.__counter = null
@@ -66,13 +69,45 @@ class App extends Component {
       hourPerDay,
       now,
       numberOfPeople,
+      showSetting,
       startTime,
-      unitPrice,
+      unitPricePerDay,
+      unitPricePerMonth,
+      unitPriceType,
     } = this.state
     
     const elapsedTime = !!now && !!startTime ? (now - startTime) : 0
-    const pricePerSecond = unitPrice / dayPerMonth / hourPerDay / 60 / 60
+    let pricePerSecond
+    if (unitPriceType === 'perDay') {
+      pricePerSecond = unitPricePerDay / 60 / 60
+    } else {
+      pricePerSecond = unitPricePerMonth / dayPerMonth / hourPerDay / 60 / 60
+    }
     const price = elapsedTime / 1000 * pricePerSecond * numberOfPeople
+
+    let setting
+    if (showSetting) {
+      setting = (
+        <div>
+          <UnitPriceType {...{
+            onChange: (s) => this.setState(s),
+            unitPriceType,
+          }} />
+          {unitPriceType === 'perMonth' && <UnitPricePerMonth {...{
+            dayPerMonth,
+            hourPerDay,
+            numberOfPeople,
+            onChange: (s) => this.setState(s),
+            unitPricePerMonth,
+          }}/>}
+          {unitPriceType === 'perDay' && <UnitPricePerDay {...{
+            onChange: (s) => this.setState(s),
+            numberOfPeople,
+            unitPricePerDay
+          }}/>}
+        </div>
+      )
+    }
 
     return (
       <div>
@@ -87,9 +122,141 @@ class App extends Component {
           {!this.__counter && !!now && <button onClick={this.__onClickResume}>続行</button>}
           {!this.__counter && !!startTime && <button onClick={this.__onClickReset}>リセット</button>}
         </div>
+        <ToggleSetting {...{
+          onChange: (s) => this.setState(s),
+          showSetting,
+        }}/>
+        {setting}
       </div>
     )
   }
+}
+
+const ToggleSetting = (props) => {
+  let text = '設定を表示する'
+  if (props.showSetting) {
+    text = '設定を隠す'
+  }
+  return (
+    <a {...{
+      href: '#',
+      onClick: (e) => {
+        e.preventDefault()
+        props.onChange({showSetting: !props.showSetting})
+      },
+    }}>
+      {text}
+    </a>
+  )
+}
+
+const UnitPriceType = (props) => {
+  return (
+    <Fragment>
+      <div>
+        <label htmlFor={'perMonth'}>
+          <input {...{
+            checked: props.unitPriceType === 'perMonth' ? 'checked' : '',
+            id: 'perMonth',
+            onChange: () => props.onChange({unitPriceType: 'perMonth'}),
+            type: 'radio',
+            value: 'perMonth',
+          }} />
+          月単価
+        </label>
+        <label htmlFor={'perDay'}>
+          <input {...{
+            checked: props.unitPriceType === 'perDay' ? 'checked' : '',
+            id: 'perDay',
+            onChange: () => props.onChange({unitPriceType: 'perDay'}),
+            type: 'radio',
+            value: 'perDay',
+          }} />
+          時給
+        </label>
+      </div>
+    </Fragment>
+  )
+}
+
+const UnitPricePerMonth = (props) => {
+  return (
+    <Fragment>
+      <div>
+        <label htmlFor={'hourPerDay'}>
+          人数
+        </label>
+        <input {...{
+          id: 'numberOfPeople',
+          onChange: (e) => props.onChange({numberOfPeople: e.target.value}),
+          type: 'input',
+          value: props.numberOfPeople,
+        }}/>
+      </div>
+      <div>
+        <label htmlFor={'hourPerDay'}>
+          日/月
+        </label>
+        <input {...{
+          id: 'dayPerMonth',
+          onChange: (e) => props.onChange({dayPerMonth: e.target.value}),
+          type: 'input',
+          value: props.dayPerMonth,
+        }}/>
+      </div>
+      <div>
+        <label htmlFor={'hourPerDay'}>
+          時間/日
+        </label>
+        <input {...{
+          id: 'hourPerDay',
+          onChange: (e) => props.onChange({hourPerDay: e.target.value}),
+          type: 'input',
+          value: props.hourPerDay,
+        }}/>
+      </div>
+      <div>
+        <label htmlFor={'hourPerDay'}>
+          月単価
+        </label>
+        <input {...{
+          id: 'unitPricePerMonth',
+          onChange: (e) => props.onChange({unitPricePerMonth: e.target.value}),
+          type: 'input',
+          value: props.unitPricePerMonth,
+        }}/>
+      </div>
+    </Fragment>
+  )
+}
+
+const UnitPricePerDay = (props) => {
+  return (
+    <div>
+      <div>
+        <label htmlFor={'hourPerDay'}>
+          人数
+        </label>
+        <input {...{
+          id: 'numberOfPeople',
+          onChange: (e) => props.onChange({numberOfPeople: e.target.value}),
+          type: 'input',
+          value: props.numberOfPeople,
+        }}/>
+      </div>
+      <div>
+        <label htmlFor={'hourPerDay'}>
+          時給
+        </label>
+        <input {...{
+          id: 'unitPricePerDay',
+          onChange: (e) => props.onChange({unitPricePerDay: e.target.value}),
+          type: 'input',
+          value: props.unitPricePerDay,
+        }}/>
+      </div>
+    </div>
+  )
 }
 
 export default App
